bed51aa786a155cfa327d137e06ccdfa329ac6f8
Antoś Bućko <30908515+Paladin-Dranser@users.noreply.github.com>
2021-12-17 18:24:26 +0300

    feat: update Firefox to the latest version

--- script/firefox_update.sh
+++ script/firefox_update.sh
@@ -1,22 +1,40 @@
-# This script update firefox browser from ~/Download/firefox*tar.bz2
 #!/bin/bash
 
-dir=/home/dranser/Downloads/
-firefox=$(find "$dir" -maxdepth 1 -name "firefox-*.tar.bz2")
+# Update Firefox browser to the latest version
 
-if ! [ $(id -u) = 0 ]; then
-    echo "The script need to be run as root." >&2
+readonly FIREFOX_ARCHIVE='/tmp/firefox.tar.bz2'
+readonly FIREFOX_DIRECTORY='/opt/firefox'
+
+if [ "$(id -u)" != '0' ]; then
+    echo "ERROR: The script need to be run as root." >&2
     exit 1
 fi
 
-if [ -e "$firefox" ]; then
-    if [ -d "/opt/firefox" ]; then
-        rm -rf /opt/firefox/
-    fi
+echo 'Downloading the latest version of firefox archive:'
+wget --output-document="${FIREFOX_ARCHIVE}" \
+    --tries=10 \
+    --no-verbose \
+    --show-progress \
+    'https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-GB'
 
-    tar -jxf "$firefox" -C /opt
-    rm -f "$firefox" 
-    echo "Firefox is updated!" >&2
-else
-    echo "There isn't a new version of firefox in $dir!" >&2
+if [ $? -ne 0 ]; then
+    echo 'ERROR: The firefox archive can not be downloaded! Existing...'
+    exit 1
 fi
+
+if [ -d "${FIREFOX_DIRECTORY}" ]; then
+    rm --recursive \
+        --force \
+        "${FIREFOX_DIRECTORY}"
+fi
+
+echo 'Extracting the firefox archive:'
+tar --extract \
+    --bzip2 \
+    --file="${FIREFOX_ARCHIVE}" \
+    --directory='/opt/' \
+    --checkpoint='.400'
+
+rm --force "${FIREFOX_ARCHIVE}" 
+
+echo -e '\nFirefox is updated!'
